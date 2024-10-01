5d10506615dec6c2b5977f3a5b6e4a58bcf0a9dc
farihafari <fareehajabeen62@gmail.com>
2024-10-01 12:49:43 +0500

    product crud done

--- dashmin/php/query.php
+++ dashmin/php/query.php
@@ -2,6 +2,7 @@
 include("connection.php");
 // addresses
 $categoryImageAddress = 'img/categories/';
+$proImageAddress = "img/products/";
 if(isset($_POST['addCategory'])){
     $catName = $_POST['catName'];
     $catImageName= $_FILES['catImage']['name'];
@@ -73,5 +74,74 @@ if(isset($_GET['delete'])){
         alert('category deleted successfully');
         location.assign('viewcategory.php')
         </script>";
+}
+// addProduct
+if(isset($_POST['addProduct'])){
+    $pName = $_POST['proName'];
+    $pPrice = $_POST['proPrice'];
+    $pQuantity = $_POST['proQuantity'];
+    $pDescription = $_POST['proDescription'];
+    $pCatId = $_POST['proCatId'];
+$pImageName = $_FILES['proImage']['name'];
+$pTmpImageName = $_FILES['proImage']['tmp_name'];
+$extension = pathinfo($pImageName,PATHINFO_EXTENSION);
+$filePath = 'img/products/'.$pImageName;
+if($extension=="jpg" || $extension =="jpeg" || $extension ="png" || $extension =="webp"){
+    if(move_uploaded_file($pTmpImageName,$filePath)){
+        $query = $pdo ->prepare("INSERT INTO `products`( `productName`, `productPrice`, `productQuantity`, `productCatId`, `productDescription`, `productImage`) VALUES(:pn,:pp,:pq,:pcid,:pd,:pi)");
+        $query->bindParam("pn",$pName);
+        $query->bindParam("pp",$pPrice);
+        $query->bindParam("pq",$pQuantity);
+        $query->bindParam("pcid",$pCatId);
+        $query->bindParam("pd",$pDescription);
+        $query->bindParam("pi",$pImageName);
+$query->execute();
+echo "<script>alert('product added into table')</script>";
+
+    }
+}
+
+}
+// updateProduct
+if(isset($_POST['updateProduct'])){
+    $pId = $_POST['proId'];
+    $pName = $_POST['proName'];
+    $pPrice = $_POST['proPrice'];
+    $pQuantity = $_POST['proQuantity'];
+    $pDescription = $_POST['proDescription'];
+    $pCatId = $_POST['proCatId'];
+    if(!empty($_FILES['proImage']['name'])){
+        $pImageName = $_FILES['proImage']['name'];
+$pTmpImageName = $_FILES['proImage']['tmp_name'];
+$extension = pathinfo($pImageName,PATHINFO_EXTENSION);
+$filePath = 'img/products/'.$pImageName;
+if($extension=="jpg" || $extension =="jpeg" || $extension ="png" || $extension =="webp"){
+    if(move_uploaded_file($pTmpImageName,$filePath)){
+        $query = $pdo ->prepare("update products set productName = :pn,productPrice = :pp , productQuantity = :pq , productDescription = :pd , productCatId = :pcid , productImage = :pi where productId = :pid");
+        $query->bindParam("pid",$pId);
+        $query->bindParam("pn",$pName);
+        $query->bindParam("pp",$pPrice);
+        $query->bindParam("pq",$pQuantity);
+        $query->bindParam("pcid",$pCatId);
+        $query->bindParam("pd",$pDescription);
+        $query->bindParam("pi",$pImageName);
+$query->execute();
+echo "<script>alert('product Updated into table')</script>";
+
+    }
+}
+
+    }else{
+        $query = $pdo ->prepare("update products set productName = :pn,productPrice = :pp , productQuantity = :pq , productDescription = :pd , productCatId = :pcid  where productId = :pid");
+        $query->bindParam("pid",$pId);
+        $query->bindParam("pn",$pName);
+        $query->bindParam("pp",$pPrice);
+        $query->bindParam("pq",$pQuantity);
+        $query->bindParam("pcid",$pCatId);
+        $query->bindParam("pd",$pDescription);
+       
+$query->execute();
+echo "<script>alert('product Updated into table')</script>";
+    }
 }
 ?>
--- dashmin/product.php
+++ dashmin/product.php
@@ -1,55 +1,58 @@
 <?php
 include("components/header.php");
 ?>
-         <!-- Blank Start -->
-         <div class="container-fluid pt-4 px-4">
-            <!-- Button trigger modal -->
+<!-- Blank Start -->
+<div class="container-fluid pt-4 px-4">
+  <!-- Button trigger modal -->
 
-                <div class="row  bg-light rounded mx-0">
-                    <div class="col-lg-12">
-                    <button type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-Add Product
-</button>
-                    </div>
-                    <div class="col-md-12">
-                        <h3 class="my-3 mx-3">All Categories</h3>
-                        <table class="table my-3">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Category Id</th>
-                                        <th scope="col">Categor Name</th>
-                                        <th scope="col">Category Image</th>
-                                        <th scope="col" colspan="2">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                  <?php
-                                  $query = $pdo ->query("select * from categories");
-                                  $row = $query->fetchAll(PDO::FETCH_ASSOC);
-                                
-                                  foreach($row as $catRows){
-                                    ?>
-                                  
-                                  <tr>
-                                        <th scope="row"><?php echo $catRows['catId']?></th>
-                                        <td><?php echo $catRows['catName']?></td>
-                                        <td><img src="<?php echo $categoryImageAddress.$catRows['catImage']?>" width="80"></td>
-                                        <td><a href="updatecategory.php?cid=<?php echo $catRows['catId']?>" class="btn btn-success">Edit</a></td>
-                                        <td><a href="?delete=<?php echo $catRows['catId']?>" class="btn btn-danger">Delete</a></td>
-                                    </tr>
-                                  <?php  
-                                  }
-                                  ?>  
-                                   
-                                    
-                                </tbody>
-                            </table>
-                    </div>
-                </div>
-            </div>
-            <!-- Blank End -->
-            <!--add  Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="row  bg-light rounded mx-0">
+    <div class="col-lg-12">
+      <button type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Add Product
+      </button>
+    </div>
+    <div class="col-md-12">
+      <h3 class="my-3 mx-3">All Products</h3>
+      <table class="table my-3">
+        <thead>
+          <tr>
+            <th scope="col">Products Id</th>
+            <th scope="col">Products Name</th>
+            <th scope="col">Products Price</th>
+            <th scope="col">Products Quantity</th>
+            <th scope="col">Products Description</th>
+            <th scope="col">Products Category</th>
+            <th scope="col">Products Image</th>
+            <th scope="col" colspan="2">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $query = $pdo->query("SELECT `products`.*, `categories`.`catName`
+FROM `products` 
+	inner JOIN `categories` ON `products`.`productCatId` = `categories`.`catId`;");
+          $row = $query->fetchAll(PDO::FETCH_ASSOC);
+
+          foreach ($row as $proRows) {
+            ?>
+
+            <tr>
+              <th scope="row"><?php echo $proRows['productId'] ?></th>
+              <td><?php echo $proRows['productName'] ?></td>
+              <td><?php echo $proRows['productPrice'] ?></td>
+              <td><?php echo $proRows['productQuantity'] ?></td>
+              <td><?php echo $proRows['productDescription'] ?></td>
+              <td><?php echo $proRows['catName'] ?></td>
+              <td><img src="<?php echo $proImageAddress . $proRows['productImage'] ?>" width="80"></td>
+              <td><a href="#modupdate<?php echo $proRows['productId']?>" data-bs-toggle="modal" class="btn btn-success">Edit</a></td>
+              <td><a href="" class="btn btn-danger">Delete</a></td>
+            </tr>
+
+
+
+            <!--update  Modal -->
+<div class="modal fade" id="modupdate<?php echo $proRows['productId']?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+  aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -57,59 +60,140 @@ Add Product
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-      <form class="mx-3" method="post" enctype="multipart/form-data">
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Product name</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1"
-                                        aria-describedby="emailHelp" name="proName">
-                                   
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Product Price</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1"
-                                        aria-describedby="emailHelp" name="proPrice">
-                                   
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Product Quantity</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1"
-                                        aria-describedby="emailHelp" name="proQuantity">
-                                   
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Product Description</label>
-                                    <textarea class="form-control" 
-                                    id="floatingTextarea" style="height: 100px;" name="proDescription"></textarea>
-                                   
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Product Category</label>
-                                    <select class="form-select" id="floatingSelect"
-                                    aria-label="Floating label select example" name="proCatId">
+        <form class="mx-3" method="post" enctype="multipart/form-data">
+          <input type="hidden" name="proId" value="<?php echo $proRows['productId']?>" id="">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product name</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="proName" value="<?php echo $proRows['productName']?>">
 
-                                    <option selected>Open this select menu</option>
-                                    <?php
-                                    $querycat = $pdo ->query("select * from categories");
-                                    $rowcat  = $querycat->fetchAll(PDO::FETCH_ASSOC);
-                                    foreach($row as $cat){
-                                        ?>
-                                        
-                                        <option value="<?php echo $cat['catId']?>"><?php echo $cat['catName']?></option>
-                                        <?php
-                                    }
-                                    ?>
-                                  
-                                    
-                                </select>
-                                   
-                                </div>
-                                <div class="mb-3">
-                                       <label for="exampleInputPassword1" class="form-label">Product image</label>
-                                    <input type="file" class="form-control" id="exampleInputPassword1" name="proImage">
-                                </div>
-                               
-                                <button type="submit" class="btn btn-primary" name="addProduct">Add Category</button>
-                            </form>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product Price</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+              name="proPrice" value="<?php echo $proRows['productPrice']?>">
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product Quantity</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+              name="proQuantity" value="<?php echo $proRows['productQuantity']?>">
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product Description</label>
+            <textarea class="form-control" id="floatingTextarea" style="height: 100px;"
+              name="proDescription" value="<?php echo $proRows['productDescription']?>"></textarea>
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product Category</label>
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="proCatId">
+
+              <option selected>Open this select menu</option>
+              <?php
+              $querycat = $pdo->query("select * from categories");
+              $rowcat = $querycat->fetchAll(PDO::FETCH_ASSOC);
+              foreach ($rowcat as $cat) {
+                ?>
+
+                <option value="<?php echo $cat['catId'] ?>" <?=$proRows['productCatId']==$cat['catId'] ? "selected" : ""?> ><?php echo $cat['catName'] ?></option>
+                <?php
+              }
+              ?>
+
+
+            </select>
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Product image</label>
+            <input type="file" class="form-control" id="exampleInputPassword1" name="proImage">
+            <img src="<?php echo $proImageAddress.$proRows['productImage']?>" width="90" alt="">
+          </div>
+
+          <button type="submit" class="btn btn-primary" name="updateProduct">Update Product</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+      </div>
+    </div>
+  </div>
+</div>
+     <!--update  Modal -->
+            <?php
+          }
+          ?>
+
+
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+<!-- Blank End -->
+<!--add  Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+  aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">ADD PRODUCTS</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form class="mx-3" method="post" enctype="multipart/form-data">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product name</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="proName">
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product Price</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+              name="proPrice">
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product Quantity</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+              name="proQuantity">
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product Description</label>
+            <textarea class="form-control" id="floatingTextarea" style="height: 100px;"
+              name="proDescription"></textarea>
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product Category</label>
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="proCatId">
+
+              <option selected>Open this select menu</option>
+              <?php
+              $querycat = $pdo->query("select * from categories");
+              $rowcat = $querycat->fetchAll(PDO::FETCH_ASSOC);
+              foreach ($row as $cat) {
+                ?>
+
+                <option value="<?php echo $cat['catId'] ?>"><?php echo $cat['catName'] ?></option>
+                <?php
+              }
+              ?>
+
+
+            </select>
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Product image</label>
+            <input type="file" class="form-control" id="exampleInputPassword1" name="proImage">
+          </div>
+
+          <button type="submit" class="btn btn-primary" name="addProduct">Add Product</button>
+        </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
